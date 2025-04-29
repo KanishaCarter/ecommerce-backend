2327eae9337ff916c9797f53063d5ce3364d896c
KanishaCarter <kanishacarter90@outlook.com>
2025-04-29 14:28:33 -0500

    created index.js to set up associations

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Product = require('./Product');
+const Cart = require('./Cart');
+const CartItem = require('./CartItem');
+
+// Associations
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
+module.exports = { User, Product, Cart, CartItem };
